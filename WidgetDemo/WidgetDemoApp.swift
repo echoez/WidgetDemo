2c1367445aa43e98ffe0b34ca7df268191f316e5
Michael Cadet <mcadet@uncommongoods.com>
2023-12-03 22:33:21 -0500

    Initial Commit

---
+++ WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by Michael Cadet on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
